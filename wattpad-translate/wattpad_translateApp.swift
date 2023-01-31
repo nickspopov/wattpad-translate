cdc5a5d7e5af746fdc7cbd358d048166db64cecf
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2023-02-01 00:47:55 +0200

    Initial Commit

---
+++ wattpad-translate/wattpad_translateApp.swift
@@ -0,0 +1,17 @@
+//
+//  wattpad_translateApp.swift
+//  wattpad-translate
+//
+//  Created by Николай Попов on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct wattpad_translateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
